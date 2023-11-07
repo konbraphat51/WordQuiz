2174b51af21bd15f3f0bc051b4d6f4c0fbb0ece9
Konbraphat <101827492+konbraphat51@users.noreply.github.com>
2023-11-07 22:24:45 +0900

    Create LanguageSelection.vue

---
+++ src/components/LanguageSelection.vue
@@ -0,0 +1,12 @@
+<template>
+    <select v-model="$i18n.locale">
+        <option value="en">English</option>
+        <option value="ja">日本語</option>
+    </select>
+</template>
+
+<script>
+    export default {
+        name: 'LanguageSelection'
+    }
+</script>
