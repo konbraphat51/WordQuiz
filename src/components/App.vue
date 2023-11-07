b1601b288175d30de575d3ea1b2e782777b09b78
Konbraphat <101827492+konbraphat51@users.noreply.github.com>
2023-11-07 22:24:17 +0900

    Create App.vue

---
+++ src/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <LanguageSelection />
+    {{ t("example.explain") }}
+</template>
+
+<script>
+    export default Vue.defineComponent({
+        name: 'App',
+        components: {
+            "LanguageSelection": Vue.defineAsyncComponent(() => loadModule("src/components/LanguageSelection.vue", options))
+        },
+        setup() {
+            //set up i18n
+            const { t } = VueI18n.useI18n()
+            return { t }
+        },
+    })
+</script>
+
+<i18n>
+    {
+        "en": {
+            "example": {
+                "explain": "This is how you use i18n"
+            }
+        }
+        "ja": {
+            "example": {
+                "explain": "こうやってi18nを使います"
+            }
+        }
+    }
+</i18n>
