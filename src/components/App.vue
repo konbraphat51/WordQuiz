419b05a06eb6e6b4940ccc90a4a314a9aecc3710
konbraphat51 <konbraphat@gmail.com>
2023-11-17 17:13:51 +0900

    Fix: i18n format

--- src/components/App.vue
+++ src/components/App.vue
@@ -4,17 +4,17 @@
 </template>
 
 <script>
-    export default Vue.defineComponent({
-        name: 'App',
-        components: {
-            "LanguageSelection": Vue.defineAsyncComponent(() => loadModule("src/components/LanguageSelection.vue", options))
-        },
-        setup() {
-            //set up i18n
-            const { t } = VueI18n.useI18n()
-            return { t }
-        },
-    })
+export default Vue.defineComponent({
+    name: 'App',
+    components: {
+        "LanguageSelection": Vue.defineAsyncComponent(() => loadModule("src/components/LanguageSelection.vue", options))
+    },
+    setup() {
+        //set up i18n
+        const { t } = VueI18n.useI18n()
+        return { t }
+    },
+})
 </script>
 
 <i18n>
@@ -23,7 +23,7 @@
             "example": {
                 "explain": "This is how you use i18n"
             }
-        }
+        },
         "ja": {
             "example": {
                 "explain": "こうやってi18nを使います"
