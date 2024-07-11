82f36fc099aaba60003c9fbf0dc6ee93615d6484
konbraphat51 <konbraphat@gmail.com>
2024-07-11 20:47:09 +0900

    change page name

---
+++ src/components/Pages/ReceiveCSVPage.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="page">
+		<h1></h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ReceiveCSVPage",
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
+}
+</script>
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,16 +1,20 @@
 const router = VueRouter.createRouter({
-    mode: "hash",
-    history: VueRouter.createWebHashHistory(),
-    routes: [
-        {
-            path: "/",
-            name: "FirstPage",
-            component: Vue.defineAsyncComponent(() => loadModule("src/components/Pages/FirstPage.vue", options))    //default page
-        },
-        {
-            path: "/second",
-            name: "SecondPage",
-            component: Vue.defineAsyncComponent(() => loadModule("src/components/Pages/SecondPage.vue", options))
-        },
-    ]
+	mode: "hash",
+	history: VueRouter.createWebHashHistory(),
+	routes: [
+		{
+			path: "/",
+			name: "ReceiveCSVPage",
+			component: Vue.defineAsyncComponent(() =>
+				loadModule("src/components/Pages/ReceiveCSVPage.vue", options),
+			), //default page
+		},
+		{
+			path: "/second",
+			name: "SecondPage",
+			component: Vue.defineAsyncComponent(() =>
+				loadModule("src/components/Pages/SecondPage.vue", options),
+			),
+		},
+	],
 })
