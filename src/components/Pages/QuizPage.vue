9f87bcb0043fc8b0f5aac0a22fd5ae6042f46592
konbraphat51 <konbraphat@gmail.com>
2024-07-11 21:05:56 +0900

    shuffle data

--- src/components/Pages/QuizPage.vue
+++ src/components/Pages/QuizPage.vue
@@ -7,5 +7,23 @@
 <script>
 export default {
 	name: "QuizPage",
+	data() {
+		return {
+			correct: [],
+			wrong: [],
+		}
+	},
+	mounted() {
+		//shuffle data
+		this.ShuffleArray(originalData)
+	},
+	methods: {
+		ShuffleArray(array) {
+			for (let i = array.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1))
+				;[array[i], array[j]] = [array[j], array[i]]
+			}
+		},
+	},
 }
 </script>
