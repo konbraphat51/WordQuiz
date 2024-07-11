3c849f18f9201b8da980799c277ca9f20308ecf8
konbraphat51 <konbraphat@gmail.com>
2024-07-11 21:21:58 +0900

    accuracy

--- src/components/Pages/ResultPage.vue
+++ src/components/Pages/ResultPage.vue
@@ -1,5 +1,26 @@
 <template>
 	<div id="page">
 		<h1>Result</h1>
+
+		<div id="Accuracy">Accuracy: {{ accuracy }}</div>
 	</div>
 </template>
+
+<script>
+export default {
+	name: "ResultPage",
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
+	data() {
+		return {
+			accuracy: 0,
+		}
+	},
+	mounted() {
+		this.accuracy = correct.length / (correct.length + wrong.length)
+	},
+}
+</script>
