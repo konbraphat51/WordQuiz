8fc3e9958b1f16b05c113ad89fa186e2e2ba7487
konbraphat51 <konbraphat@gmail.com>
2024-07-11 21:11:26 +0900

    show current word

--- src/components/Pages/QuizPage.vue
+++ src/components/Pages/QuizPage.vue
@@ -1,6 +1,9 @@
 <template>
 	<div id="page">
 		<h1>Quiz</h1>
+		<div id="WordCurrent">
+			{{ currentWord }}
+		</div>
 	</div>
 </template>
 
@@ -9,19 +12,31 @@ export default {
 	name: "QuizPage",
 	data() {
 		return {
+			quizQueue: [],
 			correct: [],
 			wrong: [],
+			index: 0,
 		}
 	},
 	mounted() {
 		//shuffle data
 		this.ShuffleArray(originalData)
+		this.quizQueue = originalData
 	},
 	methods: {
 		ShuffleArray(array) {
 			for (let i = array.length - 1; i > 0; i--) {
 				const j = Math.floor(Math.random() * (i + 1))
 				;[array[i], array[j]] = [array[j], array[i]]
+			}
+		},
+	},
+	computed: {
+		currentWord() {
+			if (this.index < this.quizQueue.length) {
+				return this.quizQueue[this.index][0]
+			} else {
+				return "..."
 			}
 		},
 	},
