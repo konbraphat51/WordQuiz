cb7fb6ed32c0bf681d8117a257a1ad08bbf4c5ec
konbraphat51 <konbraphat@gmail.com>
2024-07-11 21:24:50 +0900

    mistook table

--- src/components/Pages/ResultPage.vue
+++ src/components/Pages/ResultPage.vue
@@ -3,6 +3,20 @@
 		<h1>Result</h1>
 
 		<div id="Accuracy">Accuracy: {{ accuracy }}</div>
+
+		<div id="MissTable">
+			<h2>Mistook words</h2>
+			<table>
+				<tr>
+					<th>Original</th>
+					<th>Answer</th>
+				</tr>
+				<tr v-for="word in wrong">
+					<td>{{ word[0] }}</td>
+					<td>{{ word[1] }}</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 </template>
 
@@ -17,10 +31,12 @@ export default {
 	data() {
 		return {
 			accuracy: 0,
+			wrong: [],
 		}
 	},
 	mounted() {
 		this.accuracy = correct.length / (correct.length + wrong.length)
+		this.wrong = wrong
 	},
 }
 </script>
