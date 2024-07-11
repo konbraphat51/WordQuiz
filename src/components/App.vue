d9e55e827b093918f372584ab8ec1d95d5083c48
konbraphat51 <konbraphat@gmail.com>
2024-07-11 20:35:44 +0900

    modify template

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,29 +1,30 @@
 <template>
-    <div id="app">
+	<div id="app">
+		<button @click="$router.push('/')">First Page</button>
+		<button @click="$router.push('/second')">Second Page</button>
 
-        <button @click="$router.push('/')">First Page</button>
-        <button @click="$router.push('/second')">Second Page</button>
+		<LanguageSelection />
 
-        <LanguageSelection />
+		{{ t("example.explain") }}
 
-        {{ t("example.explain") }}
-
-        <!-- view page shown here -->
-        <router-view />
-    </div>
+		<!-- view page shown here -->
+		<router-view />
+	</div>
 </template>
 
 <script>
 export default Vue.defineComponent({
-    name: 'App',
-    components: {
-        "LanguageSelection": Vue.defineAsyncComponent(() => loadModule("src/components/LanguageSelection.vue", options))
-    },
-    setup() {
-        //set up i18n
-        const { t } = VueI18n.useI18n()
-        return { t }
-    },
+	name: "App",
+	components: {
+		LanguageSelection: Vue.defineAsyncComponent(() =>
+			loadModule("src/components/LanguageSelection.vue", options),
+		),
+	},
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
 })
 </script>
 
